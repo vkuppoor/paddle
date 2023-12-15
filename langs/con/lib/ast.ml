728863318f0d269cc3df0998524705206f2834e0
Venkat_Kuppoor <venkkuppoor@gmail.com>
2023-12-15 17:25:18 -0500

    langs/con updated ast

--- langs/con/lib/ast.ml
+++ langs/con/lib/ast.ml
@@ -1,11 +1,15 @@
 type op1 = Add1 | Sub1
-type expr = Int of int | Prim1 of op1 * expr
+type datum = Integer of int | Boolean of bool
+type expr = Lit of int | Prim1 of op1 * expr | If of expr * expr * expr
 
 let rec string_of_expr e =
   match e with
-  | Int n -> "Int(" ^ string_of_int n ^ ")"
+  | Lit n -> "Int(" ^ string_of_int n ^ ")"
   | Prim1 (op, e) ->
       let op_str = match op with Add1 -> "Add1" | Sub1 -> "Sub1" in
       "Prim1(" ^ op_str ^ ", " ^ string_of_expr e ^ ")"
+  | If (e1, e2, e3) ->
+      "If(" ^ string_of_expr e1 ^ ", " ^ string_of_expr e2 ^ ", "
+      ^ string_of_expr e3 ^ ")"
 
 let print_ast ast = print_endline (string_of_expr ast)
